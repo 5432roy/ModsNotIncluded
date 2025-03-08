cbdde5e84db2b82d5d8b9189ceb2dd1f03f38d86
Shang-Yu Chan (Roy) <schan88@ucsc.edu>
2025-03-07 23:17:55 -0800

    `[Add]` OptionConfig
    OptionConfig defines options type and value for user

---
+++ HeatExchangeNotIncluded_StorageBin/OptionConfig.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using PeterHan.PLib.Options;
+
+namespace HeatExchangeNotIncluded_StorageBin
+{
+    [JsonObject(Newtonsoft.Json.MemberSerialization.OptIn)]
+    [ConfigFile(SharedConfigLocation: true)]
+    [RestartRequired]
+    public class OptionConfig
+    {
+        public static OptionConfig Instance { get; set; } = new OptionConfig();
+
+        [Option("Required Insulite to build the insulate storage bin")]
+        [JsonProperty]
+        public bool RequiredInsulie { get; set; } = true;
+
+        [Option("Storages prvent items from emitting gas")]
+        [JsonProperty]
+        public bool Sealed { get; set; } = true;
+
+        [Option("Insulate Storage Bin Capacity", "Set the intensity level between 1 and 200.")]
+        [Limit(1, 200)]
+        [JsonProperty]
+        public int StorageCapacity { get; set; } = 100;
+    }
+}

02aab19a42c04d54836c7409bdcd76305ba1b578
Shang-Yu Chan (Roy) <schan88@ucsc.edu>
2025-03-07 23:26:39 -0800

    `[Add]` skeleton mod as a template for option menu

---
+++ OptionMenu/ModConfig.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using PeterHan.PLib.Options;
+
+namespace OptionMenu
+{
+    [JsonObject(Newtonsoft.Json.MemberSerialization.OptIn)]
+    [ConfigFile(SharedConfigLocation: true)]
+    public class ModConfig
+    {
+        public static ModConfig Instance { get; set; } = new ModConfig();
+
+        [Option("Enable Feature", "Toggle the feature on or off.")]
+        [JsonProperty]
+        public bool EnableFeature { get; set; } = true;
+
+        [Option("Feature Intensity", "Set the intensity level between 1 and 200.")]
+        [Limit(1, 200)]
+        [JsonProperty]
+        public int FeatureIntensity { get; set; } = 100;
+    }
+}
---
+++ OptionMenu/OptionMenuMod.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using KMod;
+using PeterHan.PLib.Core;
+using PeterHan.PLib.Options;
+
+namespace OptionMenu
+{
+    public class OptionMenuMod : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+            PUtil.InitLibrary();
+            new POptions().RegisterOptions(this, typeof(ModConfig));
+        }
+    }
+}

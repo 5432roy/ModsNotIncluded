a633bd74f86b5c1a698331cb22bbb1b0cd88bae3
Shang-Yu Chan (Roy) <schan88@ucsc.edu>
2025-03-08 23:07:02 -0800

    `[Add]` New mod that remove all supply errands in the game
    Why removes it? Because it is a very bad design.
    Errands that only belongs to supply errands are move to the storage errands
    
    ### Some info for furture fix if needed
    Those definition can be found in `ChoreTypes.cs` under
    
    ` public ChoreTypes(ResourceSet parent)`

---
+++ NoMoreSupplyErrand/NoMoreSupplyErrandMod.cs
@@ -0,0 +1,31 @@
+﻿using KMod;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SuitMechnisimFix
+{
+    class SuitMechanism_Patch : UserMod2
+    {
+        [HarmonyPatch(typeof(ChoreType))]
+        [HarmonyPatch(MethodType.Constructor, typeof(string), typeof(ResourceSet), typeof(string[]), typeof(string), typeof(string), typeof(string), typeof(string), typeof(IEnumerable<Tag>), typeof(int), typeof(int))]
+        public static class Patch_ChoreType_Constructor
+        {
+            [HarmonyPrefix]
+            public static void Prefix(ref string[] chore_groups)
+            {
+                if (chore_groups != null && chore_groups.Length > 0)
+                {
+                    if (chore_groups.Length == 1 && chore_groups[0] == "Hauling")
+                    {
+                        chore_groups = new string[] { "Storage" };
+                    }
+                    else if (chore_groups.Contains("Hauling"))
+                    {
+                        chore_groups = chore_groups.Where(chore => chore != "Hauling").ToArray();
+                    }
+                }
+            }
+        }
+    }
+}
